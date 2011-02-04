6dd3936c83072329bb69af27dde218aa23766d45
Cindy Zamora <cindy@ubuntu.(none)>
2011-02-03 19:44:14 -0600

    haciendo primer commit de lab3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab3::Application.config.secret_token = 'b791c2e5c0d12f8aa816a2b497c6c44c4c3f8b0b1ba7c363f07e3cf421b934c546903cbf3886ff48de48235dd8a727538ce84bb8ccebbca8ab386275b713758d'
